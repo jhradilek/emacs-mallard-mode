b4d3b5017eea928a63b0f0a8916117e3533eed38
Jaromir Hradilek <jhradilek@gmail.com>
2013-08-24 03:11:04 +0200

    Improved the documentation string for the mode definition..

--- mallard-mode.el
+++ mallard-mode.el
@@ -172,7 +172,26 @@ When BUFFER is not specified or is nil, use the current buffer."
 
 ;;;###autoload
 (define-derived-mode mallard-mode nxml-mode "Mallard"
-  "A major mode for editing Mallard files."
+  "A major mode for editing Mallard files.
+
+To start mallard-mode, either open a file with the `.page' or `.page.stub'
+file extension, or run `M-x mallard-mode' to enable it for the current
+buffer. Once enabled, mallard-mode loads the RELAX NG schema for Mallard,
+sets appropriate indentation rules, and enables automatic line wrapping.
+
+mallard-mode inherits commands and key bindings from `nxml-mode'.
+In addition, it defines a number of commands and key bindings that integrate
+it with the `yelp-check' utility in order to provide maximum comfort when
+editing Mallard pages. These commands are as follows:
+
+\\[mallard-comments] or `M-x mallard-comments' displays editorial comments.
+\\[mallard-hrefs] or `M-x mallard-hrefs' displays broken external links.
+\\[mallard-status] or `M-x mallard-status' displays the current status.
+\\[mallard-validate] or `M-x mallard-validate' validates the current buffer.
+`M-x mallard-version' displays the version of this major mode.
+
+You can customize the behavior of some of these commands by running the
+`M-x customize-mode` command."
   (setq-default indent-tabs-mode nil)
   (setq-default tab-width 2)
   (setq-default fill-column 80)
